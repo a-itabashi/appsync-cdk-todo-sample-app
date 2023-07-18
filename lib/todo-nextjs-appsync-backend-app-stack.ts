3ee5dab54fe4f7f4ad4be9f83eeb927c8888a946
Aguru itabashi <itabashi.a@gmail.com>
2023-07-18 22:24:32 +0900

    feat:AppSyncの仮実装

--- lib/todo-nextjs-appsync-backend-app-stack.ts
+++ lib/todo-nextjs-appsync-backend-app-stack.ts
@@ -1,16 +1,78 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export class TodoNextjsAppsyncBackendAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB
+    const todoTable = new dynamodb.Table(this, "TodoTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoNextjsAppsyncBackendAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // AppSync
+    const todoApi = new appsync.GraphqlApi(this, "TodoGraphqlApi", {
+      name: "todo-graphql-api",
+      schema: appsync.SchemaFile.fromAsset(
+        path.join(__dirname, "../graphql/schema.graphql")
+      ),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
+        },
+      },
+    });
+
+    // Lambda function
+    const getTodosLambda = new lambdaNodeJs.NodejsFunction(
+      this,
+      "getTodosHandler",
+      {
+        entry: path.join(__dirname, "../lambda/getTodos.ts"),
+        handler: "handler",
+        environment: {
+          TODO_TABLE: todoTable.tableName,
+        },
+        // runtimeの指定がないとAppSync側でエラーがt出力される。
+        runtime: lambda.Runtime.NODEJS_18_X,
+      }
+    );
+
+    todoTable.grantReadData(getTodosLambda);
+
+    // DataSource
+    const getTodosDataSource = todoApi.addLambdaDataSource(
+      "getTodosDataSource",
+      getTodosLambda
+    );
+
+    new appsync.Resolver(this, "QueryGetTodosResolver", {
+      api: todoApi,
+      dataSource: getTodosDataSource,
+      typeName: "Query",
+      fieldName: "getTodos",
+    });
+
+    // CfnOutput
+    new cdk.CfnOutput(this, "GraphQlApiUrl", {
+      value: todoApi.graphqlUrl,
+    });
+
+    new cdk.CfnOutput(this, "GraphQlApiKey", {
+      value: todoApi.apiKey || "",
+    });
   }
 }
